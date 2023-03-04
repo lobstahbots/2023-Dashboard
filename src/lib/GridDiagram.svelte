db2b96a91fac5bdd2211518c1e3aff5e166a07d0
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2023-03-04 12:04:34 -0500

    Add GridDiagram and NodeIcon components

---
+++ src/lib/GridDiagram.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type Node from "./Node";
+  import NodeIcon from "./NodeIcon.svelte";
+  
+  export let nodes: Node[];
+</script>
+
+<div class="grid">
+  {#each nodes as node}
+  <div>
+    <NodeIcon type={node.type} state={node.state} />
+  </div>
+  {/each}
+</div>
+
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
+</style>
---
+++ src/lib/NodeIcon.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { NodeState, NodeType } from "./Node";
+
+  import coneUnscored from "../assets/cone-unscored.svg";
+  import coneSelected from "../assets/cone-selected.svg";
+  import coneScored from "../assets/cone-scored.svg";
+  import cubeUnscored from "../assets/cube-unscored.svg";
+  import cubeSelected from "../assets/cube-selected.svg";
+  import cubeScored from "../assets/cube-scored.svg";
+  import hybridUnscored from "../assets/hybrid-unscored.svg";
+  import hybridSelected from "../assets/hybrid-selected.svg";
+  import hybridScored from "../assets/hybrid-scored.svg";
+
+  const srcMap = {
+    [NodeType.Cone]: {
+      [NodeState.Unscored]: coneUnscored,
+      [NodeState.Selected]: coneSelected,
+      [NodeState.Scored]: coneScored,
+    },
+    [NodeType.Cube]: {
+      [NodeState.Unscored]: cubeUnscored,
+      [NodeState.Selected]: cubeSelected,
+      [NodeState.Scored]: cubeScored,
+    },
+    [NodeType.Hybrid]: {
+      [NodeState.Unscored]: hybridUnscored,
+      [NodeState.Selected]: hybridSelected,
+      [NodeState.Scored]: hybridScored,
+    },
+  } as const;
+
+  export let state: NodeState;
+  export let type: NodeType;
+
+  $: src = srcMap[type][state];
+</script>
+
+<img src={src} alt="{NodeState[state]} {NodeType[type]}" />
