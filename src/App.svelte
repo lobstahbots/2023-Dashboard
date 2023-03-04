0c2c9d882ea09178d6585e7b442d47020ccfae47
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2023-03-04 12:39:22 -0500

    Add GridDiagram instance to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
+  import GridDiagram from "./lib/GridDiagram.svelte";
+  import Node, { NodeState } from "./lib/Node";
+  
+  const placeholderState: Node[] = [];
 
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 9; col++) {
+      placeholderState.push(new Node(
+        row,
+        col,
+        NodeState.Unscored
+      ));
+    }
+  }
+
+  placeholderState[0].state = NodeState.Selected;
 </script>
 
 <main>
-
+  <GridDiagram nodes={placeholderState} />
 </main>
 
 <style>
 
+  main {
+    display: grid;
+    place-items: center;
+    height: 100%;
+    overflow: auto;
+    padding: 0 3em;
+  }
 </style>
