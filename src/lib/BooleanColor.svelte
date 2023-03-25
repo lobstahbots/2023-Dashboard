62fbf38e8fa70db5a6d7fe43074e0a319d6f5ff3
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2023-03-24 22:38:28 -0400

    Add BooleanColor component

---
+++ src/lib/BooleanColor.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let value: boolean;
+  export let label: string = "";
+</script>
+
+<div class:true={value}>
+    {label}
+</div>
+
+<style>
+    div {
+        min-height: 3em;
+        padding: 1em;
+        border-radius: 1em;
+        display: grid;
+        place-items: center;
+
+        background-color: #FF0000;
+    }
+
+    .true {
+        background-color: #00FF00;
+    }
+</style>
